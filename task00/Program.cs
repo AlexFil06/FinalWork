ff3ebc5f8b2acbf5fbf04fdc0b2ffe73c62d26e8
Alex <alexfil0606@gmail.com>
2022-12-25 11:52:32 +0300

    CreatrArray

--- task00/Program.cs
+++ task00/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] CreatrArrayString()
+{
+    Console.Write("Введите число для определения размера массива  ");
+    int arg = Convert.ToInt32(Console.ReadLine());
+
+    string[] arr = new string[arg];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write("Введите строку  ");
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
+
+string[] array = CreatrArrayString();
