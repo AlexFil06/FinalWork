3208ec348d642812db1e7d20801090221a470e02
Alex <alexfil0606@gmail.com>
2022-12-25 11:54:21 +0300

    Main method

--- task00/Program.cs
+++ task00/Program.cs
@@ -13,3 +13,25 @@
 }
 
 string[] array = CreatrArrayString();
+
+
+string[] Sorting(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length >= 3) count++;
+    }
+
+    string[] newArr = new string[count];
+    int j = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length >= 3)
+        {
+            newArr[j] = arr[i];
+            j++;
+        }
+    }
+    return newArr;
+}
